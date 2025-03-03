577f187a94f0bf1dc14641fd9fbcf7b9516abbed
Richard Tuharsky <tuharsky.richard@gmail.com>
2025-03-03 06:56:09 +0100

    x

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
